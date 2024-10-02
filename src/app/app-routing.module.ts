e4d5675e278ab9ea9617e008b1fa998026543cbd
CHINTHALA DHEERAJ <dheerajchinthaladev@gmail.com>
2024-10-02 22:56:26 +0530

    Added Page Modules And Components And Added  Auth Modules And Components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'Page', loadChildren: () => import('./page/page.module').then(m => m.PageModule) }, { path: 'Auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) }];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/page/page.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { PageRoutingModule } from './page-routing.module';
+import { PageComponent } from './page.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    PageComponent,
+    HeaderComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    PageRoutingModule
+  ]
+})
+export class PageModule { }
